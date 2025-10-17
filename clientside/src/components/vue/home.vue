e2214b3e3d6193e99132fa55caf773cef7848353
Miaouceo <lalienestmauve@gmail.com>
2025-10-17 16:46:18 +0200

    removed unnecessary

--- clientside/src/components/vue/home.vue
+++ clientside/src/components/vue/home.vue
@@ -4,9 +4,6 @@
        Introduction:
     </h1>
   
-As part of this project, we chose the theme of cinema. Because we really love movies and we are quite film enthusiasts. We therefore decided to create a database focused on films. To make our movie database better, we want to add much more detailed information about the main subjects: actors and directors. For every director, we will include a special ID, their name, their birth details and nationality, and facts about their career like how many films they made. We will do the same for actors and their nominations.  Spectator will therefore be able to make reviews of the films they have seen.
-
-
   </div>
   <div>
     Kino is CInema in hungarian.
